40975b094ab07984276408eba90b9b477ffaf706
NewtonHub1 <g1066776677@gmail.com>
2026-07-07 15:16:08 +0545

    Initial commit

---
+++ Question1.java
@@ -0,0 +1,6 @@
+public class Question1 {
+    public static void main(String[] args) {
+        String studentName = "Newton";
+        System.out.println(studentName);
+    }
+}
---
+++ Question2.java
@@ -0,0 +1,14 @@
+public class Question2 {
+    public static void main(String[] args){
+        String StudentIdString = "12345";
+        System.out.println("Student ID:"+StudentIdString);
+        String AgeString = "16";
+        System.out.println("Age:"+AgeString);
+        String Gpa = "3.8";
+        System.out.println("GPA:"+Gpa);
+        String Address ="Nepal pokhara 25 hemja";
+        System.out.println("Address:"+Address);
+        
+        
+    }
+}
